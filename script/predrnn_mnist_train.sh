8be3234ed13dc2347bb5948f78f0fb4d0812c2d0
Luna314 <43090521+Luna314@users.noreply.github.com>
2021-07-13 20:44:00 +0800

    Add files via upload

---
+++ script/predrnn_mnist_train.sh
@@ -0,0 +1,31 @@
+cd ..
+python -u run.py \
+    --is_training 1 \
+    --device cuda \
+    --dataset_name mnist \
+    --train_data_paths /home/ices/PycharmProject/LUNA/moving-mnist-train.npz \
+    --valid_data_paths /home/ices/PycharmProject/LUNA/moving-mnist-valid.npz \
+    --save_dir checkpoints/mnist_predrnn \
+    --gen_frm_dir results/mnist_predrnn \
+    --model_name predrnn \
+    --reverse_input 1 \
+    --img_width 64 \
+    --img_channel 1 \
+    --input_length 10 \
+    --total_length 20 \
+    --num_hidden 64,64,64,64 \
+    --filter_size 5 \
+    --stride 1 \
+    --patch_size 4 \
+    --layer_norm 1 \
+    --scheduled_sampling 1 \
+    --sampling_stop_iter 50000 \
+    --sampling_start_value 1.0 \
+    --sampling_changing_rate 0.00002 \
+    --lr 0.0003 \
+    --batch_size 8 \
+    --max_iterations 10000 \
+    --display_interval 50 \
+    --test_interval 1000 \
+    --snapshot_interval 1000\
+    --pretrained_model checkpoints/mnist_predrnn/model.ckpt-20000
